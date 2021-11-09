20a0ea31387b23a0dfbb5f90bbcabc8503ddc582
kaeppen <seho@itu.dk>
2021-11-09 11:23:25 +0100

    Import + client adjustments

--- client.go
+++ client.go
@@ -3,15 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
-	"https://github.com/kaeppen/disys-mandatory2/DiMutex"
+
+	d "github.com/kaeppen/disys-mandatory2/dimutex"
 	"google.golang.org/grpc"
 )
 
-
 type diMutexClient struct {
-	cc grpc.ClientConnInterface
-	state State
-	timestamp int 
+	cc        grpc.ClientConnInterface
+	state     State
+	timestamp int
 }
 
 const (
@@ -19,15 +19,20 @@ const (
 )
 
 func main() {
+	//hvordan skal opsætning af
+
+	c := diMutexClient{}
 	//Initialize with state = released
-	state = Released
-	timestamp = 0 //init logical clock to 0
+	c.state = Released
+	c.timestamp = 0 //init logical clock to 0
 	fmt.Println("fuck imports")
 
 }
 
-func (c *diMutexClient) RequestAccess(ctx context.Context, in *AccessRequest, opts ...grpc.CallOption) (*AccessGrant, error) {
-	//kode her :) 
-	c.state = Wanted //Set the state to wanted 
-	
+func (c *diMutexClient) RequestAccess(ctx context.Context, in *d.AccessRequest, opts ...grpc.CallOption) (*d.AccessGrant, error) {
+	//kode her :)
+	c.state = Wanted //Set the state to wanted
+
+	//compiler gets happy
+	return nil, nil
 }
