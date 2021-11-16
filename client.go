47800698a3f3f657a1074c935fa7f07186458248
kaeppen <seho@itu.dk>
2021-11-16 22:51:21 +0100

    remove cluttering log statements

--- client.go
+++ client.go
@@ -205,7 +205,6 @@ func setupConnection(c *diMutexClient) {
 				log.Fatalf("Could not connect: %s", err)
 			}
 			c.peers[i] = d.NewDiMutexClient(conn)
-			log.Printf("node %v added node %v to list of peers", c.id, i)
 		}
 	}
 }
