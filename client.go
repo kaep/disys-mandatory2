0c255a9c975602ac12541f8288680aefb82900c9
kaeppen <seho@itu.dk>
2021-11-16 21:17:09 +0100

    lots of different changes - not far from home

--- client.go
+++ client.go
@@ -17,11 +17,13 @@ import (
 type diMutexClient struct {
 	d.UnimplementedDiMutexServer
 	state     State
-	timestamp int
+	timestamp int32
 	ctx       context.Context
-	peers     []d.DiMutexClient //bliver det et problem med denne type som jo ikke har cluster, state osv.?
+	peers     map[int]d.DiMutexClient
 	name      string
-	id        int
+	id        int32
+	queue     []int32
+	replies   int32
 }
 
 func main() {
@@ -36,7 +38,8 @@ func main() {
 	log.SetOutput(wrt)
 	c := diMutexClient{}
 	c.name = os.Getenv("NAME")
-	c.id, _ = strconv.Atoi(os.Getenv("ID"))
+	id, _ := strconv.Atoi(os.Getenv("ID"))
+	c.id = int32(id)
 	c.state = Released
 	c.timestamp = 0
 	ctx, cancel := context.WithCancel(context.Background())
@@ -54,6 +57,16 @@ func main() {
 	}
 	go serve(server, listen, &c)
 
+	//set up connection to peers
+	setupConnection(&c)
+
+}
+
+func demo() {
+	//create some demo behavior here
+}
+
+func test(c *diMutexClient) {
 	//testkode som sender en grpc request
 	if c.id != 0 {
 		conn, err := grpc.Dial("dimutex_1:8080", grpc.WithInsecure(), grpc.WithBlock())
@@ -75,7 +88,6 @@ func main() {
 
 		}
 	}
-
 }
 
 func serve(server *grpc.Server, listener net.Listener, c *diMutexClient) {
@@ -95,50 +107,65 @@ func (c *diMutexClient) Hello(ctx context.Context, in *d.Empty) (*d.Empty, error
 func GetAccess(message string, c *diMutexClient) {
 	c.timestamp++    //bump logical clock
 	c.state = Wanted //set the state to wanted
-	request := &d.AccessRequest{Message: "I want access!", Lamport: int32(c.timestamp), Id: 9000}
+	request := &d.Request{Message: "I want access!", Lamport: int32(c.timestamp), Id: c.id}
 	c.RequestAccess(c.ctx, request)
 }
 
 //The "multicast" part of the algorithm
-func (c *diMutexClient) RequestAccess(ctx context.Context, in *d.AccessRequest) (*d.AccessGrant, error) {
-	log.Printf("%v requesting access to cs", c.name)
+func (c *diMutexClient) RequestAccess(ctx context.Context, in *d.Request) (*d.Empty, error) {
+	log.Printf("%v (node %v) requesting access to cs", c.name, c.id)
 	//bump own clock before sending out the message
 	c.timestamp++
 	//set own state to wanted
 	c.state = Wanted
 
-	replies := 0
+	//tell my peers that i want the critical section
 	peers := c.peers
 	for i := 0; i < len(peers); i++ {
 		answer, err := peers[i].AnswerRequest(ctx, in)
 		if answer != nil && err != nil {
-			replies++
+			c.replies++
 		}
 	}
 	//if i receive N-1 replies, then i can have the critical section
-	if replies == len(peers) {
+	if int(c.replies) == len(peers) {
 		c.HoldAndRelease(ctx, &d.Empty{})
+	} else {
+		//wait for replies.. might be possible with streaming?
 	}
 
 	//compiler gets happy -> revisit return
-	return nil, nil
+	return &d.Empty{}, nil
 }
 
-func (c *diMutexClient) HoldAndRelease(ctx context.Context, empty *d.Empty) (*d.Empty, error) {
-	log.Printf("%v has gotten access to cs", c.name)
+func (c *diMutexClient) HoldAndRelease(ctx context.Context, empty *d.Empty) (*d.Reply, error) {
+	log.Printf("%v (node %v) has gotten access to cs", c.name, c.id)
 	c.state = Held
 	//Hold the critical section for 7 seconds
 	time.Sleep(7 * time.Second)
 	//Release it
 	c.state = Released
-	log.Printf("%v has released cs", c.name)
+	log.Printf("%v (node %v) has released cs", c.name, c.id)
 
-	//maybe broadcast
+	//reply to my queue
+	c.ReplyToQueue()
 
-	return empty, nil
+	return &d.Reply{}, nil
 }
 
-func hasPrecedence(ownTime int, ownId int, otherTime int, otherId int) bool {
+func (c *diMutexClient) ReplyToQueue() {
+	for i := 0; i < len(c.queue); i++ {
+		reply := d.Reply{Message: fmt.Sprintf("Node %v replying to node %v's request", c.id, c.queue[i]), Lamport: c.timestamp, Id: c.id}
+		c.peers[int(c.queue[i])].Grant(c.ctx, &reply)
+	}
+}
+
+func (c *diMutexClient) Grant(ctx context.Context, reply *d.Reply) (*d.Empty, error) {
+
+	return &d.Empty{}, nil
+}
+
+func hasPrecedence(ownTime int32, ownId int32, otherTime int32, otherId int32) bool {
 	if ownTime > otherTime {
 		return true
 	} else if otherTime > ownTime {
@@ -153,29 +180,31 @@ func hasPrecedence(ownTime int, ownId int, otherTime int, otherId int) bool {
 	}
 }
 
-func (c *diMutexClient) AnswerRequest(ctx context.Context, request *d.AccessRequest) (*d.RequestAnswer, error) {
+func (c *diMutexClient) AnswerRequest(ctx context.Context, request *d.Request) (*d.Reply, error) {
 	log.Print("Jeg er lige blevet ringet op med et gRPC kald, av av")
 	c.timestamp++ //increment before doing anything
 	//if this node already has access or wants it and also has "more right"
-	if c.state == Held || (c.state == Wanted && hasPrecedence(c.timestamp, c.id, int(request.Lamport), int(request.Id))) {
+	if c.state == Held || (c.state == Wanted && hasPrecedence(c.timestamp, c.id, request.Lamport, request.Id)) {
 		//queue the request from other node
-		return nil, nil //return value?
+		c.queue = append(c.queue, request.Id)
+		return nil, nil
 	} else {
 		//else, just send a reply
-		answer := &d.RequestAnswer{}
-		return answer, nil
+		reply := d.Reply{Message: fmt.Sprintf("Node %v replying to node %v's request", c.id, request.Id), Lamport: c.timestamp, Id: c.id}
+		return &reply, nil
 	}
 }
 
 //Set up connection to peers
 func setupConnection(c *diMutexClient) {
-	//harcode connection to peers
 	for i := 0; i < 4; i++ {
-		port := fmt.Sprintf(":&v", c.id+8080)
-		conn, err := grpc.Dial(port, grpc.WithInsecure(), grpc.WithBlock())
-		if err != nil {
-			log.Fatalf("Could not connect: %s", err)
+		port := fmt.Sprintf("dimutex_%v:%v", i+1, i+8080)
+		if int32(i) != c.id {
+			conn, err := grpc.Dial(port, grpc.WithInsecure(), grpc.WithBlock())
+			if err != nil {
+				log.Fatalf("Could not connect: %s", err)
+			}
+			c.peers[i] = d.NewDiMutexClient(conn)
 		}
-		c.peers = append(c.peers, d.NewDiMutexClient(conn))
 	}
 }
